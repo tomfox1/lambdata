b224a9aef85bf6ca7edae89f1eb930800d48b73c
Tomas Alexander Fox <TomasFox@Tomass-MacBook-Air.local>
2019-05-30 00:33:58 -0300

    Add helper function

---
+++ lambdata_tomfox1/what_to_do.py
@@ -0,0 +1,23 @@
+class Cake: 
+    """example"""
+    x=2
+
+    def __init__(self):
+        self.baked = False
+
+    def bake(self):
+        self.baked = True
+        print("All done!")
+
+class Datacleaner: 
+    """"class for cleaning our data"""
+
+    def __init__(self, df):
+        self.df = df
+    def replace_values(self, value=np.nan, new=0)
+        """replace values in a dataframe""" 
+        self.df = self.df.replace(value, new)
+    def impute_values(self):
+        pass
+
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-tomfox1",
+    version="0.3.1",
+    author="tomfox1",
+    description="A collection of Data Science Helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomfox1/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
