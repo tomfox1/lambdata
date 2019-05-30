17b266d2071cf6cd2753dca91528548bad79afe1
Tomas Alexander Fox <TomasFox@Tomass-MacBook-Air.local>
2019-05-30 00:44:36 -0300

    updating __intit__.py to update help function

--- lambdata_tomfox1/__init__.py
+++ lambdata_tomfox1/__init__.py
@@ -7,6 +7,7 @@ lambdata - a collection of Data Science helper functions
 import pandas as pd
 import numpy as np
 from . import example_module
+from . import what_to_do
 
 Y = example_module.increment(example_module.x)
 TEST = pd.DataFrame(np.ones(10))
